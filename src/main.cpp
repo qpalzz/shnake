4d414064aed49d8c59027c31be851ee08a46cf6b
Тараканов Вадим <qpalzz@mail.ru>
2011-10-21 23:33:41 +0700

    Добавлено главное меню

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@ gboolean timeout_draving_cb(gpointer data);
 gboolean timeout_moving_cb(gpointer data);
 void SetProjectionMatrix(GLsizei, GLsizei);
 void SetModelviewMatrix();
+GtkWidget* create_menubar();
+GtkWidget* create_my_menuitem(GtkWidget *menu,gchar *labelmenu,gchar *labelmenu_r,gchar *active);
+void  menuitem_response (GtkMenuItem *menuitem,gpointer user_data);
 
 int main(int, char**);
 
@@ -139,10 +142,49 @@ void SetModelviewMatrix(){
 	glLoadIdentity();
 }
 
+static GtkItemFactoryEntry menu_items[] = {
+  { "/_Игра",               NULL,         NULL,           0, "<Branch>" },
+  { "/Игра/_Новая игра",    "<control>N", NULL,           0, "<Item>" },
+  { "/Игра/_Пауза",         "Pause",      NULL,           0, "<Item>" },
+  { "/Игра/разд1",          NULL,         NULL,           0, "<Separator>" },
+  { "/Игра/Результаты",     NULL,         NULL,           0, "<Item>" },
+  { "/Игра/Закончить игру", NULL,         NULL,           0, "<Item>" },
+  { "/Игра/разд2",          NULL,         NULL,           0, "<Separator>" },
+  { "/Игра/Выход",          "<control>Q", gtk_main_quit,  0, "<Item>" },
+  { "/_Правка",             NULL,         NULL,           0, "<Branch>" },
+  { "/Правка/Параметры",    NULL,         NULL,           1, "<Item>" },
+  { "/_Справка",            NULL,         NULL,           0, "<Branch>" },
+  { "/Справка/_О программе",NULL,         NULL,           0, "<Item>" },
+};
+static gint nmenu_items = sizeof (menu_items) / sizeof (menu_items[0]);
+
+GtkWidget *get_menubar_menu( GtkWidget  *window)
+{
+    GtkItemFactory *item_factory;
+    GtkAccelGroup *accel_group;
+    /* Создаём группу акселераторов (shortcut keys) */
+    accel_group = gtk_accel_group_new ();
+    /* Создаём ItemFactory (строку меню) */
+    item_factory = gtk_item_factory_new (GTK_TYPE_MENU_BAR, "<main>",
+                                         accel_group);
+    /* Эта функция генерирует пункты меню. Передает item factory,
+     номер пункта в массиве, массив непосредственно, а также любые
+     данные обратных вызовов для пунктов меню. */
+    gtk_item_factory_create_items (item_factory, nmenu_items, menu_items, NULL);
+    /* Прикрепляем новую группу акселераторов к окну. */
+    gtk_window_add_accel_group (GTK_WINDOW (window), accel_group);
+    /* Возвращаем фактически созданную производством пунктов (item factory) панель меню. */
+    return gtk_item_factory_get_widget (item_factory, "<main>");
+}
+
+
+// ---------------------------main()----------------------------
+
 int main(int argc, char** argv)
 {
 	GtkWidget* window;
     GtkWidget* box_main;
+    GtkWidget* menubar;
     GtkWidget* glarea;
     int attrlist[] = {
             GDK_GL_RGBA,
@@ -168,6 +210,10 @@ int main(int argc, char** argv)
     gtk_container_add (GTK_CONTAINER(window), box_main);
     gtk_widget_show (box_main);
 
+    menubar = get_menubar_menu(window);
+    gtk_box_pack_start (GTK_BOX (box_main), menubar,FALSE, TRUE, 0);
+    gtk_widget_show (menubar);
+
     glarea = gtk_gl_area_new(attrlist);
     gtk_widget_set_events(GTK_WIDGET(glarea),
                           GDK_EXPOSURE_MASK|
@@ -181,6 +227,8 @@ int main(int argc, char** argv)
     gtk_widget_set_usize(GTK_WIDGET(glarea), 600, 600);
     gtk_box_pack_start (GTK_BOX(box_main), glarea,      FALSE, TRUE, 0);
     gtk_widget_show (glarea);
+
+    gtk_widget_show_all(window);
 
     gtk_main ();
 
